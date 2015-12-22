e71aeea0da975987f68a0ea1c45b2c2c1f8cc35d
Yuriy Khabarov <13real008@gmail.com>
2015-12-22 13:46:35 +0500

    hacks for mobile IOS

--- mobile/ios/hacks.js
+++ mobile/ios/hacks.js
@@ -1,1 +1,14 @@
+// hack for safari inputs when text is not visible as you type it
+// the problem is with hardware scrolling acceleration, if it's set
 
+//if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+if (/iPhone|iPod/i.test(navigator.userAgent)) {
+    (function(){
+        $(document).on('focus', 'input', function(){
+            $('#page').css('-webkit-overflow-scrolling', 'auto');
+        });
+        $(document).on('blur', 'input', function(){
+            $('#page').css('-webkit-overflow-scrolling', '');
+        });
+    })();
+}
